292ce880da284e5c515d4c3d11489b76d6a586e7
mambax7 <mambax7@gmail.com>
2021-04-10 06:59:21 -0400

    fix missing loadAdminConfig()

--- admin/index.php
+++ admin/index.php
@@ -39,7 +39,6 @@ use Xmf\Module\Admin;
 use Xmf\Request;
 use Xmf\Yaml;
 use XoopsModules\Xoopspoll\{
-    Common,
     Common\TestdataButtons,
     Helper,
     Utility
@@ -81,27 +80,6 @@ $adminObject->addInfoBoxLine(sprintf("<span class='infolabel'>" . _MD_XOOPSPOLL_
 
 $adminObject->displayNavigation(basename(__FILE__));
 
-//------------- Test Data ----------------------------
-
-if ($helper->getConfig('displaySampleButton')) {
-    $yamlFile            = dirname(__DIR__) . '/config/admin.yml';
-    $config              = loadAdminConfig($yamlFile);
-    $displaySampleButton = $config['displaySampleButton'];
-
-    if (1 == $displaySampleButton) {
-        xoops_loadLanguage('admin/modulesadmin', 'system');
-        require_once dirname(__DIR__) . '/testdata/index.php';
-
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'LOAD_SAMPLEDATA'), '__DIR__ . /../../testdata/index.php?op=load', 'add');
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'SAVE_SAMPLEDATA'), '__DIR__ . /../../testdata/index.php?op=save', 'add');
-        //    $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'EXPORT_SCHEMA'), '__DIR__ . /../../testdata/index.php?op=exportschema', 'add');
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'HIDE_SAMPLEDATA_BUTTONS'), '?op=hide_buttons', 'delete');
-    } else {
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'SHOW_SAMPLEDATA_BUTTONS'), '?op=show_buttons', 'add');
-        $displaySampleButton = $config['displaySampleButton'];
-    }
-    $adminObject->displayButton('left', '');
-}
 
 //------------- Test Data Buttons ----------------------------
 if ($helper->getConfig('displaySampleButton')) {
